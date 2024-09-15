e88e67b6ef7d8fa91b3640e9b6733f8b7fd0ad48
kor-nelaa <czfuras2137@gmail.com>
2024-09-15 15:57:04 +0200

    tera dziaa

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function updateCountdown() {
             const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
             const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-            const countdownText = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-            document.getElementById('iledod').textContent = countdownText;
+            let countdownText = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+            document.getElementById('iledo').textContent = "Do papieżowej: " + countdownText;
         }
 
         setInterval(updateCountdown, 1000);
